9733249de6d666363273942cda6712c924b3c630
mishankov <mishankov@mail.com>
2020-04-13 19:18:51 +0300

    send POST requests payloads from todoist and trello to rabbitmq

--- app.py
+++ app.py
@@ -1,10 +1,48 @@
-from flask import Flask
+import os
+from flask import Flask, request
+import pika
 
+# RabbitMQ constants
+RABBITMQ_CONNECTION_STRING = os.getenv('RABBITMQ_CONNECTION_STRING', 'localhost')
+RABBITMQ_EXCAHNGE = 'service_callbacks'
+
+# Application
 app = Flask(__name__)
 
 
-@app.route('/api/webhooks/todoist/', methods=['GET', 'POST'])
+@app.route('/webhooks/todoist/', methods=['GET', 'POST'])
 def todoist_webhook():
+    if request.method == 'POST':
+        connection = pika.BlockingConnection(pika.URLParameters(RABBITMQ_CONNECTION_STRING))
+        channel = connection.channel()
+        channel.exchange_declare(exchange=RABBITMQ_EXCAHNGE, exchange_type='topic')
+
+        channel.basic_publish(exchange=RABBITMQ_EXCAHNGE,
+                              routing_key='todoist',
+                              body=request.data,
+                              properties=pika.BasicProperties(
+                                  delivery_mode=2
+                              ))
+        connection.close()
+
+    return 'ok'
+
+
+@app.route('/webhooks/trello/', methods=['GET', 'POST', 'HEAD'])
+def trello_webhook():
+    if request.method == 'POST':
+        connection = pika.BlockingConnection(pika.URLParameters(RABBITMQ_CONNECTION_STRING))
+        channel = connection.channel()
+        channel.exchange_declare(exchange=RABBITMQ_EXCAHNGE, exchange_type='topic')
+
+        channel.basic_publish(exchange=RABBITMQ_EXCAHNGE,
+                              routing_key='trello',
+                              body=request.data,
+                              properties=pika.BasicProperties(
+                                  delivery_mode=2
+                              ))
+        connection.close()
+
     return 'ok'
 
 
